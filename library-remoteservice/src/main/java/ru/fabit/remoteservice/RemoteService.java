3313553692a60668176554b85a5782f5fd1910db
Zherebtsov Alexandr <zherebtsovalexandr@gmail.com>
2022-08-05 11:26:57 +0300

    Added getJsonArray method

--- library-remoteservice/src/main/java/ru/fabit/remoteservice/RemoteService.java
+++ library-remoteservice/src/main/java/ru/fabit/remoteservice/RemoteService.java
@@ -1,5 +1,6 @@
 package ru.fabit.remoteservice;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -14,4 +15,10 @@ public interface RemoteService {
                                          final HashMap<String, Object> params,
                                          final Map<String, String> headers,
                                          final Object sender);
+
+    Observable<JSONArray> getRemoteJsonArray(final int requestMethod,
+                                             final String relativePath,
+                                             final HashMap<String, Object> params,
+                                             final Map<String, String> headers,
+                                             final Object sender);
 }
